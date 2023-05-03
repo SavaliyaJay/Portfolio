d9c2432077c25a49b97317f1279297a97450d844
Jay Savaliya <jrsavaliya04@gmail.com>
2023-05-03 14:57:03 +0530

    update 5

--- pages/portfolio/[id].js
+++ pages/portfolio/[id].js
@@ -1,32 +1,72 @@
 import React from "react";
-import installNode from "../../public/assets/portfolio/installNode.jpg";
-import reactParallax from "../../public/assets/portfolio/reactParallax.jpg";
-import usestate from "../../public/assets/portfolio/usestate.jpg";
-import reactWeather from "../../public/assets/portfolio/reactWeather.jpg";
-import img1 from "../../public/assets/portfolio/Starbuck/1.png";
-import img2 from "../../public/assets/portfolio/React News App Function/1.png";
 import { FaGithub } from "react-icons/fa";
 import { AiOutlineCaretRight } from "react-icons/ai";
 import { BiChevronLeft } from "react-icons/bi";
 import Link from "next/link";
 import Image from "next/image";
 
+// only all image part 
+import img1_1 from "../../public/assets/portfolio/Starbuck/1.png";
+import img1_2 from "../../public/assets/portfolio/Starbuck/2.png";
+import img1_3 from "../../public/assets/portfolio/Starbuck/3.png";
+
+import img2_1 from "../../public/assets/portfolio/React News App Function/1.png";
+import img2_2 from "../../public/assets/portfolio/React News App Function/2.png";
+import img2_3 from "../../public/assets/portfolio/React News App Function/3.png";
+
+import img3_1 from "../../public/assets/portfolio/PHP project/1.jpg";
+
+import img4_1 from "../../public/assets/portfolio/React Project/1.png";
+
+import img5_1 from "../../public/assets/portfolio/Tailwind project/1.png";
+import img5_2 from "../../public/assets/portfolio/Tailwind project/2.png";
+
+
 const portfolios = [
   {
     id: 1,
     title: "Starbuks",
-    imageSrc: img1,
+    imageSrc: [img1_1, img1_2, img1_3],
     url: "Starbuks",
     tech: "Html,CSS,Javascript",
+    desc: "This is my simple javascript project in which I will click on any glass on the screen, the web-page will change its color image.",
+    projecturl: "https://github.com/SavaliyaJay"
   },
   {
     id: 2,
     title: "News App",
-    imageSrc: img2,
+    imageSrc: [img2_1, img2_2, img2_3],
     url: "NewsApp",
     tech: "React JS,Boostrap",
+    desc: "Using React js i will create a news web in which i use react-infinite-scroll-component and react-top-loading-bar. ",
+    projecturl: "https://github.com/SavaliyaJay/News-App-Using-Function-Base-Component"
   },
-  
+  , {
+    id: 3,
+    title: "PHP Project Book-Store",
+    imageSrc: [img3_1],
+    url: "phpProject",
+    tech: "Html, CSS, bootstrap, Javascript, PHP, SQL",
+    desc: "This system is used for online advertising the books for sale and purchase.The user can upload the pictures of book along with the price and little description.The enquiry system has been added to this system.The chat messaging part like an email facility has been encoded with the site.This system provides the administrator with full - fledged facility of removing abusive books or malicious users.It provides the administrator to add books, remove and contact It takes care of authentication of the the user who have registered to the site. Administrator, user and guest user",
+    projecturl: "https://github.com/SavaliyaJay/book-store"
+  }, {
+    id: 4,
+    title: "E-learning",
+    imageSrc: [img4_1],
+    url: "eLearning",
+    tech: "React Js, Tailwind CSS, Node JS, Express JS, Mongoosee",
+    desc: "This is my React JS Web App. This is E learning Platform using which tutor can sell their course, Students can learn from that course. ",
+    projecturl: "https://github.com/SavaliyaJay/Elearning_FrontEnd"
+  }, {
+    id: 5,
+    title: "Tailwind CSS Project",
+    imageSrc: [img5_1, img5_2],
+    url: "tailCss",
+    tech: "HTML, Tailwind CSS",
+    desc: "This my fully responsive talwind css project in which i create a responsive navbar,and all other things",
+    projecturl: "https://github.com/SavaliyaJay/Only_tailwind"
+  }
+
 ];
 
 const getPortfolioFrom = (url) => portfolios.filter((p) => p.url === url)[0];
@@ -50,24 +90,26 @@ export async function getStaticProps({ params }) {
   };
 }
 
-const OnePortfolio = ({ portfolio: { title, imageSrc } }) => {
+const OnePortfolio = ({ portfolio: { title, imageSrc, desc } }) => {
   return (
-    <div className="h-fit w-full text-center">
+    <div className="h-fit w-full text-center bg-[#f3e8ff]">
       <div className="max-w-screen-xl mx-auto w-full h-full pt-24 p-8 flex flex-col">
         <div className="flex">
           <Link href="/portfolio">
-            <div className="flex items-center justify-center my-8 text-indigo-500 font-bold capitalize cursor-pointer">
+            <div className="flex items-center justify-center my-8 text-indigo-900 font-bold capitalize cursor-pointer">
               <BiChevronLeft size={25} /> back
             </div>
           </Link>
         </div>
 
-        <h1 className="capitalize text-4xl mt-2 mb-8 text-center md:text-left font-bold text-blue-500 tracking-wider">
+        <h1 className="capitalize text-4xl mt-2 mb-8 text-center md:text-left font-bold text-indigo-600 tracking-wider">
           {title}
         </h1>
 
-        <div className="relative w-96 h-56 mx-auto overflow-hidden my-8">
-          <Image src={imageSrc} alt="bla" layout="fill" objectFit="cover" />
+        <div className="max-w-6xl mx-auto grid gap-8 ">
+          {imageSrc.map((img, index) => (
+            <Image key={index} src={img} alt="bla" className="w-full h-full object-cover cursor-pointer rounded-md bg-white p-3 " />
+          ))}
         </div>
 
         <h2 className="text-center md:text-left my-4 text-2xl font-bold">
@@ -75,32 +117,26 @@ const OnePortfolio = ({ portfolio: { title, imageSrc } }) => {
         </h2>
 
         <p>
-          Human thy god sainted is the distinctly shrieked grave bird shrieked,
-          yore borrow nevermore but gaunt maiden have nothing darkness, answer
-          evermore word there from raven, sorrowsorrow from on and nevermore of
-          core. Whom the meant and while i of respiterespite his lamplight. Or
-          being weary parting grave lent if. Rare came stillness whether the
-          lord. Bust myself whom of soul silken. The that there this my, quaint
-          that only lenore stern muttered and nothing or tapping. Here lie
-          lining i still. A tis some and chamber flitting. Lamplight radiant
-          from no yet what, upon that that one decorum leave.
+          {desc}
         </p>
 
+
+
         <div className="flex items-center justify-center gap-10">
-          <Link href="/">
-            <div className="group flex items-center justify-center my-8 bg-blue-500 text-white px-6 py-3 font-bold uppercase rounded-md tracking-wider cursor-pointer">
+          <Link href="">
+            <div className="group flex items-center justify-center my-8 text-white bg-[#7c3aed]  hover:bg-[#5b21b6]  px-6 py-3 font-bold uppercase rounded-md tracking-wider cursor-pointer">
               demo
               <span className="duration-200 ease-in">
-                <AiOutlineCaretRight size={18} className="ml-2" />
+                <FaGithub size={18} className="ml-2" />
               </span>
             </div>
           </Link>
 
-          <Link href="https://github.com/theyashpatel">
-            <div className="group flex items-center justify-center my-8 bg-blue-500 text-white px-6 py-3 font-bold uppercase rounded-md tracking-wider cursor-pointer">
+          <Link href="/">
+            <div className="group flex items-center justify-center my-8 text-white bg-[#7c3aed]  hover:bg-[#5b21b6] px-6 py-3 font-bold uppercase rounded-md tracking-wider cursor-pointer">
               my portfolio
               <span className="duration-200 ease-in">
-                <FaGithub size={18} className="ml-2" />
+                <AiOutlineCaretRight size={18} className="ml-2" />
               </span>
             </div>
           </Link>
--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -1,10 +1,9 @@
 import React from "react";
-import installNode from "../../public/assets/portfolio/installNode.jpg";
-import reactParallax from "../../public/assets/portfolio/reactParallax.jpg";
-import usestate from "../../public/assets/portfolio/usestate.jpg";
-import reactWeather from "../../public/assets/portfolio/reactWeather.jpg";
 import img1 from "../../public/assets/portfolio/Starbuck/1.png";
 import img2 from "../../public/assets/portfolio/React News App Function/1.png";
+import img3 from "../../public/assets/portfolio/PHP project/2.jpg";
+import img4 from "../../public/assets/portfolio/React Project/2.png";
+import img5 from "../../public/assets/portfolio/Tailwind project/3.png";
 import { MdExpandMore } from "react-icons/md";
 import Link from "next/link";
 import Image from "next/image";
@@ -16,16 +15,34 @@ export const getStaticProps = async () => {
       title: "Star Bucks",
       imageSrc: img1,
       url: "Starbuks",
-      tech: "Html,CSS,Javascript",
+      tech: "Html, CSS, Javascript",
     },
     {
       id: 2,
       title: "News App",
       imageSrc: img2,
       url: "NewsApp",
-      tech: "React JS,Boostrap",
-    },
-    
+      tech: "React JS, Boostrap",
+    }, {
+      id: 3,
+      title: "PHP Project Book-Store",
+      imageSrc: img3,
+      url: "phpProject",
+      tech: "Html, CSS, bootstrap, Javascript, PHP, SQL",
+    }, {
+      id: 4,
+      title: "E-learning",
+      imageSrc: img4,
+      url: "eLearning",
+      tech: "React Js, Tailwind CSS, Node JS, Express JS, Mongoosee",
+    }, {
+      id: 4,
+      title: "Tailwind CSS Project",
+      imageSrc: img5,
+      url: "tailCss",
+      tech: "HTML, Tailwind CSS",
+    }
+
   ];
 
   return {
@@ -35,25 +52,36 @@ export const getStaticProps = async () => {
 
 const PortfoliosRoute = ({ portfolios }) => {
   return (
-    <div id="portfolio" className="w-full">
+    <div id="portfolio" className="w-full bg-[#f3e8ff]">
       <div className="max-w-screen-xl mx-auto pt-24 p-4 text-center md:text-left">
-        <h2 className="text-5xl md:text-7xl tracking-wider uppercase text-blue-500 font-bold pb-16">
-          portfolio
-        </h2>
+        <div className="flex flex-col justify-center items-center">
+          <h2 className="text-3xl md:text-5xl tracking-wider text-indigo-950 font-bold pb-5">
+            Portfolio
+          </h2>
+          <h3 className="text-xl md:text-3xl tracking-wider text-indigo-950 font-bold pb-10">
+            My All Projects
+          </h3>
+        </div>
 
         <div className="max-w-5xl mx-auto grid md:grid-cols-2 gap-8">
-          {portfolios.map(({ id, title, imageSrc, url }) => (
+          {portfolios.map(({ id, title, imageSrc, url, tech }) => (
             <Link key={id} href={`/portfolio/${url}`}>
-              <div className="cursor-pointer group shadow-md shadow-gray-600 overflow-hidden rounded-md">
+              <div className="cursor-pointer overflow-hidden rounded-md bg-white px-2 pt-2">
                 <Image
                   src={imageSrc}
                   alt={title}
                   className="rounded-md duration-200 
-              hover:scale-110"
+                  hover:scale-110"
                 />
-                <h2 className="text-center text-base capitalize my-4 font-light duration-200 group-hover:underline underline-offset-4">
-                  {title}
-                </h2>
+                <div className="flex justify-between items-center mx-4">
+                  <div className="my-4">
+                    <h2 className="text-center text-base  capitalize font-bold text-indigo-950">{title}</h2>
+                    <h3 className="text-center text-base capitalize font-bold text-indigo-950">technologies: {tech}</h3>
+                  </div>
+                  <div className="group flex items-center justify-center my-2 text-[#6d28d9] hover:border-b-4 hover:border-indigo-500 px-6 py-2 font-bold cursor-pointer">
+                    Read More
+                  </div>
+                </div>
               </div>
             </Link>
           ))}
