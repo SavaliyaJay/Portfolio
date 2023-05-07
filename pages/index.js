a351fbc7b558a9d45b08873ecdc8e309687cf665
Jay Savaliya <jrsavaliya04@gmail.com>
2023-05-07 11:15:54 +0530

    add my heading

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Portfolio from '@/components/Portfolio'
 import Experience from '@/components/Experience'
 import Me from '@/components/Me'
 import Contact from '@/components/Contact'
+import Head from 'next/head'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -12,6 +13,9 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <div className='bg-[#f3e8ff]'>
+      <Head>
+        <title>Jay - Portfolio</title>
+      </Head>
       <HomePage />
       <Portfolio />
       <Experience />
